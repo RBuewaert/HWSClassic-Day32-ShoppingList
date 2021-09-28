180fe50b162d7dc199819b0ef41ab6955f1d5c47
Romain Buewaert <romain.buewaert@gmail.com>
2021-09-28 12:59:17 +0200

    Initial Commit

---
+++ Project6M-Milestone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6M-Milestone
+//
+//  Created by Romain Buewaert on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
