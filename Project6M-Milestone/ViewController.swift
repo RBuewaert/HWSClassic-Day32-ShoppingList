77f4eebb793f84bfbe36964f083c3a1204bcfdf4
Romain Buewaert <romain.buewaert@gmail.com>
2021-09-28 15:03:23 +0200

    first commit

--- Project6M-Milestone/ViewController.swift
+++ Project6M-Milestone/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        title = "Your shopping list"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Remove All", style: .done, target: self, action: #selector(removeList))
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter an item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+
+            self?.shoppingList.insert(answer, at: 0)
+
+            let indexPath = IndexPath(row: 0, section: 0)
+            self?.tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+
+    @objc func removeList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
 }
 
